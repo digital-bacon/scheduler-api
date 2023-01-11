b47ee41b82ed4bc132b9819932503776ffeda6cc
Mark Bieganek <mark.bieganek@telus.com>
2023-01-11 15:30:19 -0700

    Use axios to get data from the iTunes API and render results with <Results>

--- react-albums/src/components/LiveSearch.js
+++ react-albums/src/components/LiveSearch.js
@@ -1,4 +1,5 @@
-import React, { Fragment, useState } from "react";
+import React, { useEffect, Fragment, useState } from "react";
+import axios from "axios"
 
 import SearchBar from "components/SearchBar";
 import Results from "components/Results";
@@ -6,6 +7,13 @@ import Results from "components/Results";
 export default function LiveSearch(props) {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const url = `https://search-itunes.vercel.app/?term=${term}&country=CA&media=music&entity=album&attribute=artistTerm`;
+    axios.get(url)
+      .then((response) => setResults(response.data.results))
+      .catch((error) => console.log('Axios responded with an error: ', error.message));
+  }, [term]);
 
   return (
     <Fragment>
